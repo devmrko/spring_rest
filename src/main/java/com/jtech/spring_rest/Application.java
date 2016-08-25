f6b6623cdaae3a9d56eae3b386509979d73f1aa2
devmrko <devmrko@gmail.com>
2016-08-25 15:34:51 +0900

    add hello world example

--- src/main/java/com/jtech/spring_rest/Application.java
+++ src/main/java/com/jtech/spring_rest/Application.java
@@ -1,11 +1,18 @@
 package com.jtech.spring_rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class);
+	
+	private final static Logger LOG = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		LOG.info(">>>>> >>>>> >>>>> >>>>> >>>>> main");
+		SpringApplication.run(Application.class, args);
 	}
+	
 }
---
+++ src/main/java/com/jtech/spring_rest/Greeting.java
@@ -0,0 +1,26 @@
+package com.jtech.spring_rest;
+
+public class Greeting {
+
+	private final long id;
+	private final String content;
+
+    /**
+     *
+     * @param id
+     * @param content
+     */
+    public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
